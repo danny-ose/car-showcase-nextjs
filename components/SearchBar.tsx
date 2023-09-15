3049fc44df406f5e17de38cd149f8f7c49f50f1f
Daniel Mathew <dustincarter8334>
2023-09-15 18:52:29 +0100

    implemented navigation and footer components

---
+++ components/SearchBar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+const SearchBar = () => {
+  const handleSearch = () => {};
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
